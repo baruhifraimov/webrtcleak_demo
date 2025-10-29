f09134004229e0f0424b6eaa5b480eeea1560c4f
Baruh Ifraimov <Baruh.Ifraimov@gmail.com>
2025-10-30 00:10:04 +0200

    Update version number in script.js

--- webrtc-leak-demo-js/script.js
+++ webrtc-leak-demo-js/script.js
@@ -1,4 +1,4 @@
-// 23102025.0017
+// 30102025.0009
 // This regex is designed to match all standard IPv4 formats.
 const regexIPv4 = /\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/;
 // This is a more comprehensive regex for IPv6 that handles various formats including compressed ones.
